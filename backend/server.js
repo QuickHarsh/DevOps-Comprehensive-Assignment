25d852520d4185811e55fd380fcee99878c01db7
Harsh Patel <hp1659414@gmail.com>
2026-04-26 11:15:18 +0530

    added path of index.html backend and created docker

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,12 @@ app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
+const frontendPath = path.join(__dirname,"..","frontend","dist");
+app.use(express.static(frontendPath));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(frontendPath,"index.html"))
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
